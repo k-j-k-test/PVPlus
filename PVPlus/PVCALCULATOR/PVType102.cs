6bfecac90275d6b388c0417a490ff6f2816453d2
k-j-k <wjdhrks10@naver.com>
2025-09-29 17:36:34 +0900

    PVType102: 건강등급 로직추가

--- PVPlus/PVCALCULATOR/PVType102.cs
+++ PVPlus/PVCALCULATOR/PVType102.cs
@@ -2,7 +2,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,59 +12,45 @@ namespace PVPlus.PVCALCULATOR
 {
     public class PVType102 : PVType11
     {
-        // 우체국 든든건강종신
         public PVType102(LineInfo line) : base(line)
         {
-            for(int i = 0; i < CommutationTable.MAXSIZE; i++)
-            {
-                c.MxSegments_급부합계[i] =
-                    c.MxSegments_급부[0][i] * (c.Rate_r3[i] == 0 ? 1 : c.Rate_r2[i] / c.Rate_r3[i])
-                    + c.MxSegments_급부[1][i]
-                    + c.MxSegments_급부[2][i]
-                    + c.MxSegments_급부[3][i]
-                    + c.MxSegments_급부[4][i]
-                    + c.MxSegments_급부[5][i]
-                    + c.MxSegments_급부[6][i] * (c.Rate_r3[i] == 0 ? 1 : c.Rate_r2[i] / c.Rate_r3[i]);
-
-                c.Mx_급부[i] = c.MxSegments_급부합계[i] + c.Mx_납입자급부[i] + c.Mx_납입면제자급부[i];
-            }
+            c = FindLowestPremiumGrade();
         }
 
-        public override double Get순보험료(int n, int m, int t, int freq)
+        public CommutationTable FindLowestPremiumGrade()
         {
-            return base.Get순보험료(n, m, t, freq);
-        }
+            int grade = (int)variables["F6"];
+            int age = (int)variables["Age"];
 
-        public override double GetBeta순보험료(int n, int m, int t, int freq)
-        {
-            return base.GetBeta순보험료(n, m, t, freq);
-        }
+            int n = (int)variables["n"];
+            int m = (int)variables["m"];
+            int t = (int)variables["t"];
+            int freq = (int)variables["freq"];
 
-        public override double Get준비금(int n, int m, int t, int freq)
-        {
-            if (t == n) return 1;
+            List<(double, CommutationTable)> c_grades = new List<(double, CommutationTable)>();
 
-            double BetaNP = GetBeta순보험료(n, m, t, freq);
-            double payCnt = Get연납입횟수(freq);
-            double NNx_납입자 = GetNNx(c.Nx_납입자, c.Dx_납입자, freq, t, m);
-            double NNx_납후유지자 = GetNNx(c.Nx_유지자, c.Dx_유지자, 12, Math.Max(m, t), n);
+            //디버깅용 grades2
+            //List<(double, CommutationTable)> c_grades2 = new List<(double, CommutationTable)>();
 
-            bool Is발생자 = (int)variables["S6"] > 0 ? true : false;
-            double[] Dstar = c.GetDx(c.Rate_r2);
-            double VE = 0;
-
-            if (Is발생자)
+            for (int i = 0; i <= grade; i++)
             {
-                VE = (c.MxSegments_급부[0][t] - c.MxSegments_급부[0][n] + c.MxSegments_급부[6][t] - c.MxSegments_급부[6][n] + ex.Betaprime_S * NNx_납후유지자) / c.Dx_유지자[t];
-            }
-            else
-            {
-                VE = (c.Mx_급부[t] - c.Mx_급부[n]) / Dstar[t] + ex.Betaprime_S * NNx_납후유지자 / c.Dx_유지자[t] - BetaNP * payCnt * NNx_납입자 / Dstar[t];
+                CommutationTable c_grade = MakeCommutationTable();
+                c = c_grade;
+
+                double GP = base.Get영업보험료(n, m, t, freq);
+                c_grades.Add((GP, c_grade));
+
+                //디버깅용 준비금
+                //double V = base.Get준비금(n, m, 1, 12);
+                //c_grades2.Add((V, c_grade));
+
+                variables["Age"] = (int)variables["Age"] - 1;
             }
 
+            variables["Age"] = age; //원래 나이로 복구
 
-            return VE;
-
+            //영업보험료가 가장 낮은 등급 선택
+            return c_grades.OrderBy(x => x.Item1).First().Item2;
         }
     }
 }
